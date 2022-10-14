7506e6dc0cc79c11dbd184c1e8e69efa7c7481e5
rondido <rppr001@naver.com>
2022-10-14 18:23:26 +0900

    javascript Mission-01 end commit

--- template/app.js
+++ template/app.js
@@ -1,23 +1,26 @@
 // do something!
 
-const nav = document.querySelector("nav");
-const togglebtn = document.querySelector(".toggle");
+const main = {
+  // 캡슐화 같은 개념이다..
+  init: function () {
+    const nav = document.querySelector("nav");
+    const togglebtn = document.querySelector(".toggle");
+    const body = document.querySelector("body");
+    function handleClick() {
+      nav.classList.toggle("active");
+      body.classList.contains("preload")
+        ? body.classList.remove("preload")
+        : "";
+      nav.classList.contains("active")
+        ? localStorage.setItem("key", "active")
+        : localStorage.removeItem("key");
+    }
+    window.onload = function () {
+      localStorage.getItem("key") ? nav.classList.add("active") : "";
+      document.querySelector("body").style.visibility = "visible";
+    };
+    togglebtn.addEventListener("click", handleClick);
+  },
+};
 
-// active가 생성될때 그 값을 상태에 넣어주어야 함..
-// handleClick을 눌렀는 상태인지 아닌지 알아야 함..
-function handleClick() {
-  let active_dis = nav.classList.toggle("active");
-  //  localstoreage 에 넣고 찾아와서 값이 없으면 닫는다
-  // 값이 있다면 active을 가지고 있는 것 처럼 생각해서 구현하기
-  if (active_dis) {
-    localStorage.setItem("active", "active");
-  } else {
-    localStorage.removeItem("active");
-  }
-}
-
-let myStorage = localStorage.getItem("active");
-alert(myStorage);
-// local이 비어있으면 null
-
-togglebtn.addEventListener("click", handleClick);
+main.init();
