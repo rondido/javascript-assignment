897f9eac9679538ff8dde297866cc616433f685f
rondido <rppr001@naver.com>
2022-10-20 23:27:28 +0900

    Mission-03 star commit

--- Mission-03/template/star-rating/index.js
+++ Mission-03/template/star-rating/index.js
@@ -1,11 +1,35 @@
 // do something!
-export default function StarRating() {
-  const star = document.querySelector(".star-rating");
-  const div = document.createElement("div");
-  div.classList.add("star-rating-container");
-  star.appendChild(div);
-  const content = document.querySelector(".star-rating-container");
-  const i = document.createElement("box-icon");
-  i.tagName.add("bx", "bxs-star");
-  content.appendChild(i);
-}
+const StarRating = ($container) => {
+  const maxRating = $container.getAttribute("data-max-rating");
+  const newNode = document.createDocumentFragment();
+  const link = document.createElement("link");
+  link.href = "star-rating/theme.css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
+  for (let i = 0; i < maxRating; i++) {
+    const star = document.createElement("i");
+    star.classList.add("bx", "bxs-star");
+    star.setAttribute("data-star", `${i + 1}`);
+    newNode.appendChild(star);
+  }
+
+  $container.appendChild(newNode);
+  $container.className = "star-rating star-rating-container";
+
+  const mouse = document.querySelector(".bx");
+
+  mouse.addEventListener("mouseover", (event) => {
+    for (let i = 0; i < maxRating; i++) {
+      mouse.classList.add("hovered");
+    }
+  });
+
+  mouse.addEventListener("mouseout", (event) => {
+    mouse.classList.remove("hovered");
+  });
+  mouse.addEventListener("click", (event) => {
+    mouse.classList.add("selected");
+  });
+};
+
+export default StarRating;
