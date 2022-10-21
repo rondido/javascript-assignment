98be2a9a2270f727db841d5ed1811350bb1c372d
rondido <rppr001@naver.com>
2022-10-21 22:07:32 +0900

    event Delegation commit

--- Mission-03/template/star-rating/index.js
+++ Mission-03/template/star-rating/index.js
@@ -1,36 +1,59 @@
 // do something!
 const StarRating = ($container) => {
-  const maxRating = $container.getAttribute("data-max-rating");
-  const newNode = document.createDocumentFragment();
   const link = document.createElement("link");
   link.href = "star-rating/theme.css";
   link.rel = "stylesheet";
   document.getElementsByTagName("head")[0].appendChild(link);
-  for (let i = 0; i < maxRating; i++) {
-    const star = document.createElement("i");
-    star.classList.add("bx", "bxs-star");
-    star.setAttribute("data-star", `${i + 1}`);
-    newNode.appendChild(star);
+
+  $container.className = "star-rating star-rating-container";
+
+  const maxRating = $container.dataset.maxRating;
+  $container.innerHTML += new Array(Number(maxRating))
+    .fill(0)
+    .map(createStartTag)
+    .join(" ");
+
+  const $containerChildren = Object.entries($container.children).map(
+    (i) => i[1]
+  );
+  const rating = new starRating();
+
+  addEvent("mouseover", addEventMethod, "hovered");
+  addEvent("click", addEventMethod, "selected");
+
+  function addEvent(eventName, eventMethod, name) {
+    $containerChildren.forEach(($star, index) =>
+      $star.addEventListener(eventName, (event) =>
+        eventMethod(event, { index, name })
+      )
+    );
   }
 
-  $container.appendChild(newNode);
-  $container.className = "star-rating star-rating-container";
-  const mouse = document.getElementsByClassName(".bx");
-  //queryselector를 사용하면 무조건 하나만 찾아온다.
-  // const i_length = document.getElementsByClassName("bx");
-  //하나만적용된다? 왜???? 하나만적용될까?
-  document
-    .querySelector(".star-rating-container")
-    .addEventListener("mouseover", (e) => {
-      mouse.classList.add("hovered");
+  function addEventMethod(event, payload) {
+    $containerChildren.forEach(($item, index) => {
+      if (payload.index >= index) {
+        $item.classList.add(payload.name);
+        return;
+      }
+      $item.classList.remove(payload.name);
     });
+  }
 
-  document.querySelector(".bx").addEventListener("mouseout", (e) => {
-    mouse.classList.remove("hovered");
-  });
-  document.querySelector(".bx").addEventListener("click", (e) => {
-    mouse.classList.add("selected");
-  });
+  $container.addEventListener("mouseleave", () =>
+    $containerChildren.forEach(($item) => $item.classList.remove("hovered"))
+  );
 };
+
+function createStartTag() {
+  const tag = document.createElement("i");
+  tag.classList.add("bx", "bxs-star");
+  return tag.outerHTML;
+}
+
+class starRating {
+  constructor() {
+    this.rating = 0;
+  }
+}
 
 export default StarRating;
