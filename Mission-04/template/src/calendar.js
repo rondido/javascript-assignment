d969566b6a0baa3ac4cf14ca6c418ec67cf9702e
rondido <rppr001@naver.com>
2022-10-27 21:51:11 +0900

    Mission-04 반응형 디자인 커밋

--- Mission-04/template/src/calendar.js
+++ Mission-04/template/src/calendar.js
@@ -1,8 +1,18 @@
 import useCalculateDate from "./calculateDate.js";
 
 function useCalendar(date) {
-  const { getAllDay, getHeader, plusMonthByOne, minusMonthByOne, todayClick,pmDate,thisMonth,nmDate,clickNextDay,clickPrevDay } =
-    useCalculateDate(date);
+  const {
+    getAllDay,
+    getHeader,
+    plusMonthByOne,
+    minusMonthByOne,
+    todayClick,
+    pmDate,
+    thisMonth,
+    nmDate,
+    clickNextDay,
+    clickPrevDay,
+  } = useCalculateDate(date);
 
   const calendarHeader = document.querySelector(".header");
   const calendarBody = document.querySelector(".date");
@@ -12,7 +22,6 @@ function useCalendar(date) {
   const eventText = document.querySelector(".text");
   const calendar_header = document.querySelector(".calender");
   const toFullDay = new Date();
-  
 
   nextDiv.addEventListener("click", function () {
     plusMonthByOne();
@@ -31,42 +40,37 @@ function useCalendar(date) {
     e.target.classList.add("day");
     calendar_header.classList.remove("draw");
   });
-  
-  // eventText.addEventListener("click", function () {
-  //   calendar_header.classList.remove("draw");
-  // });
 
   window.addEventListener("click", (e) => {
-    if([calendarHeader,nextDiv,prevDiv,document.querySelector(".calendar-nav")].includes(e.target)){
-      return ;
+    if (
+      [
+        calendarHeader,
+        nextDiv,
+        prevDiv,
+        document.querySelector(".calendar-nav"),
+      ].includes(e.target)
+    ) {
+      return;
     }
     e.target == eventText
       ? calendar_header.classList.add("draw")
       : calendar_header.classList.remove("draw");
   });
-  
 
   const render = () => {
     const getYear = date.getFullYear();
     const getMonth = date.getMonth();
     const thisMonth = new Date(getYear, getMonth + 1, 0);
     const nmDate = thisMonth.getDate();
-    const prevMonth = new Date(getYear, getMonth, 0);               
-    const pmDate = prevMonth.getDate(); //이전달
-    const pmDay = prevMonth.getDay();
-    const nmYear = thisMonth.getFullYear();
-    const nmMonth = thisMonth.getMonth();
-    const nmDay = thisMonth.getDay(); //이번달 1일
-    const span = document.querySelector(".spanday");
+    const prevMonth = new Date(getYear, getMonth, 0);
     calendarHeader.textContent = getHeader();
     calendarBody.innerHTML = getAllDay()
-    .map((value, i) => {  
-
-        const condition =        
+      .map((value, i) => {
+        const condition =
           i >= getAllDay().indexOf(1) && i < getAllDay().lastIndexOf(nmDate) + 1
-            ? "this" 
+            ? "this"
             : "other";
-        
+
         return `<div class='dates'><span class='spanday ${condition}' data-day='${value}'>${value}</span></div>`;
       })
       .join("");
@@ -82,40 +86,39 @@ function useCalendar(date) {
       }
     }
     const dates = document.querySelectorAll(".dates");
-    getAllDay().forEach((value,i) =>{
-      for(let i=0; i< dates.length; i++){
+    getAllDay().forEach((value, i) => {
+      for (let i = 0; i < dates.length; i++) {
         let targetday;
-        dates[i].addEventListener("click", function(e){          
-        document.querySelector(".clickEvent")?.classList.remove("clickEvent");
-        dates[i].classList.add("clickEvent");
+        dates[i].addEventListener("click", function (e) {
+          document.querySelector(".clickEvent")?.classList.remove("clickEvent");
+          dates[i].classList.add("clickEvent");
           //이전달
-        if(i <= getAllDay().indexOf(1)){
-          if(e.target.innerHTML.length < 2 ){
-            targetday = "0" + e.target.innerHTML;
-          }else{
-            targetday = e.target.innerHTML
+          if (i <= getAllDay().indexOf(1)) {
+            if (e.target.innerHTML.length < 2) {
+              targetday = "0" + e.target.innerHTML;
+            } else {
+              targetday = e.target.innerHTML;
+            }
+            eventText.value = clickPrevDay() + targetday;
+          } else if (i < getAllDay().lastIndexOf(nmDate) + 1) {
+            if (e.target.innerHTML.length < 2) {
+              targetday = "0" + e.target.innerHTML;
+            } else {
+              targetday = e.target.innerHTML;
+            }
+            eventText.value = todayClick() + targetday;
+          } else {
+            if (e.target.innerHTML.length < 2) {
+              targetday = "0" + e.target.innerHTML;
+            } else {
+              targetday = e.target.innerHTML;
+            }
+            eventText.value = clickNextDay() + targetday;
           }
-          eventText.value=clickPrevDay() + targetday;               
-        }           
-        else if(i < getAllDay().lastIndexOf(nmDate) + 1){
-          if(e.target.innerHTML.length < 2 ){
-            targetday = "0" + e.target.innerHTML;
-          }else{
-            targetday = e.target.innerHTML
-          }
-            eventText.value=todayClick() + targetday;
-          }else{
-          if(e.target.innerHTML.length < 2 ){
-            targetday = "0" + e.target.innerHTML;
-          }else{
-            targetday = e.target.innerHTML
-          }
-          eventText.value=clickNextDay() + targetday;
-          }
-        })         
-      }   
-    })  
-  };  
+        });
+      }
+    });
+  };
   render();
 }
 
