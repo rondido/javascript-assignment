d653c6ae4d0cd87097e8315a403a863d6f4f2537
rondido <rppr001@naver.com>
2022-11-01 02:04:08 +0900

    feat: nav 카테고리 commit

--- Mission-05/template/App.js
+++ Mission-05/template/App.js
@@ -1,36 +1,28 @@
 // do something!
 // 전역 상태 관리 및 api 연결
-import Nav from './components/Nav.js';
-//import NewList from './components/NewList.js';
-  
-function App(){  
-  const apiKey = 'daca2ab1ed1f4d9f8b4cd5e05a92a669';
-  const pageSize = 5;
-  //여기서 카테고리별로 데이터를 받아와서 관리
-  const state = {
-      category:{
-        
-      }
-  }
+import Nav from "./components/Nav.js";
+import NewList from "./components/NewsList.js";
 
-  const news = async () => { 
-    try{
-        await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr&category=${category === 'all' ? '' : category}&page=${page}&pageSize=${pageSize}&apiKey=${apiKey}`,                        
-        ).then(function(response){
-          return response;
-        }).catch(function (error) {
-            console.log(error);
-        });
-    }catch(e){
-        console.log(e)
-    }
-  }
-  
-  const render = () =>{
-    Nav();
-  }
-  render();
+function App() {
+  //전역 상태
+  //observable
+  const state = {
+    all: { id: "all", title: "전체보기" },
+    business: { id: "business", title: "비지니스" },
+    entertainment: { id: "entertainment", title: "엔터테이먼트" },
+    health: { id: "health", title: "건강" },
+    science: { id: "science", title: "과학" },
+    sports: { id: "sports", title: "스포츠" },
+    technology: { id: "technology", title: "기술" },
+  };
+  const $root = document.querySelector("#root");
+  const $nav = document.createElement("nav");
+  $nav.classList.add("category-list");
+  $root.appendChild($nav);
+
+  Nav($root, state, $nav);
+
+  return { $root, state, $nav };
 }
 
 App();
--- Mission-05/template/components/NewsList.js
+++ Mission-05/template/components/NewsList.js
@@ -1,15 +1,4 @@
 // do something!
 // 뉴스리스트 나오는곳
-function NewList() {
-    const root = document.querySelector("#root");
-    const div = document.createElement("div");
-    div.classList.add("news-list-container");
-    root.append(div);
-    const aritcle_parent = document.querySelector(".news-list-container");
-    const article =  document.createElement("article");
-    article.classList.add("news-list");
-    aritcle_parent.append(article);
-    
-}
-  export default NewList;
-  
+function NewList() {}
+export default NewList;
