3e66bd895127b8827b6e545586cc1ac8ba4de4da
rondido <rppr001@naver.com>
2022-11-03 00:49:02 +0900

    feat: nav commit

--- Mission-05/template/App.js
+++ Mission-05/template/App.js
@@ -6,22 +6,47 @@ import NewList from "./components/NewsList.js";
 function App() {
   //전역 상태
   //observable
-  const state = {
-    all: { id: "all", title: "전체보기" },
-    business: { id: "business", title: "비지니스" },
-    entertainment: { id: "entertainment", title: "엔터테이먼트" },
-    health: { id: "health", title: "건강" },
-    science: { id: "science", title: "과학" },
-    sports: { id: "sports", title: "스포츠" },
-    technology: { id: "technology", title: "기술" },
-  };
+  const state = new Proxy(
+    {
+      all: { id: "all", title: "전체보기" },
+      business: { id: "business", title: "비지니스" },
+      entertainment: { id: "entertainment", title: "엔터테이먼트" },
+      health: { id: "health", title: "건강" },
+      science: { id: "science", title: "과학" },
+      sports: { id: "sports", title: "스포츠" },
+      technology: { id: "technology", title: "기술" },
+    },
+    {
+      set: function (target, prop, value) {},
+    }
+  );
+
   const $root = document.querySelector("#root");
   const $nav = document.createElement("nav");
   $nav.classList.add("category-list");
   $root.appendChild($nav);
 
+  const pageSize = 5;
+  const category = "technology";
+  const page = 5;
+  const apiKey = "daca2ab1ed1f4d9f8b4cd5e05a92a669";
+  const url = `https://newsapi.org/v2/top-headlines?country=kr&category=${
+    category === "all" ? "" : category
+  }&page=${page}&pageSize=${pageSize}&apiKey=${apiKey}`;
+  const news = async () => {
+    await axios
+      .get(url)
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  };
   Nav($root, state, $nav);
-
+  NewList(news);
   return { $root, state, $nav };
 }
 
