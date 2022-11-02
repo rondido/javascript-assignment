64f633660d508297c578c10f2e597250f4ff28b0
rondido <rppr001@naver.com>
2022-11-03 00:48:58 +0900

    feat: nav commit

--- Mission-05/template/components/Nav.js
+++ Mission-05/template/components/Nav.js
@@ -1,7 +1,5 @@
 // do something!
 
-//import NewList from "./NewsList";
-
 //카테고리별
 function Nav($root, state, $nav) {
   const $ul = document.createElement("ul");
@@ -9,6 +7,7 @@ function Nav($root, state, $nav) {
   const category = Object.entries(state);
   const navList = [];
   //객체의 키값과 요소 값을 가져옴
+
   category.forEach(([key, value]) => {
     navList.push(
       `  
@@ -19,6 +18,15 @@ function Nav($root, state, $nav) {
 
   $ul.innerHTML = navList.join("");
   $nav.appendChild($ul);
+  const $li = document.querySelectorAll("li");
+  console.log($li);
+  $li.forEach((value) => {
+    value.addEventListener("click", function (e) {
+      value.classList.add("active");
+
+      $ul.innerHTML = navList.join("");
+    });
+  });
 }
 
 export default Nav;
--- Mission-05/template/components/NewsList.js
+++ Mission-05/template/components/NewsList.js
@@ -1,4 +1,14 @@
 // do something!
 // 뉴스리스트 나오는곳
-function NewList() {}
+function NewList(news) {
+  console.log(news);
+  const $root_div = document.querySelector("#root");
+  const $div = document.createElement("div");
+  const $article = document.createElement("article");
+  const $section = document.createElement("section");
+  $root_div.append($div);
+  $div.classList.add("news-list-container");
+  $div.append($article);
+  $article.classList.add("news-list");
+}
 export default NewList;
