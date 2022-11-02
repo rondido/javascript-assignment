e2b624bff6be13dacec832d16e2b7adc88a836d5
rondido <rppr001@naver.com>
2022-11-02 18:41:44 +0900

    feat: nav modify commit

--- Mission-05/template/components/Nav.js
+++ Mission-05/template/components/Nav.js
@@ -1,30 +1,24 @@
 // do something!
+
+//import NewList from "./NewsList";
+
 //카테고리별
 function Nav($root, state, $nav) {
   const $ul = document.createElement("ul");
   // 객체의 키와 값을 담은 배열을 반환
   const category = Object.entries(state);
+  const navList = [];
   //객체의 키값과 요소 값을 가져옴
   category.forEach(([key, value]) => {
-    $ul.appendChild(Category({ state: value }));
+    navList.push(
+      `  
+        <li class="category-item" id="${value.id}">${value.title}</li>   
+      `
+    );
   });
 
+  $ul.innerHTML = navList.join("");
   $nav.appendChild($ul);
 }
 
-function Category(state) {
-  const title = state;
-  const $li = document.createElement("li");
-  $li.classList.add("category-item");
-  $li.id = title.state.id;
-  $li.innerHTML = title.state.title;
-  $li.addEventListener("click", function (e) {
-    const active = document.querySelectorAll(".active");
-    if (active !== null) {
-      $li.classList.remove("active");
-    }
-    $li.classList.add("active");
-  });
-  return $li;
-}
 export default Nav;
