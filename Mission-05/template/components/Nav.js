9e2610b3d6ff57dfac4f449b287747c2a79ac808
Park <rppr001@naver.com>
2022-11-04 02:05:04 +0900

    feat:news data get commit

--- Mission-05/template/components/Nav.js
+++ Mission-05/template/components/Nav.js
@@ -1,30 +1,58 @@
 // do something!
 
 //카테고리별
-function Nav($root, state,$nav) {
-  const $ul = document.createElement("ul");
-  // 객체의 키와 값을 담은 배열을 반환
-  const category = Object.entries(state);
-  const navList = [];
-  //객체의 키값과 요소 값을 가져옴
+export default function Nav(initialState, onClick) {
+  const $root = document.querySelector("#root");
+  this.state = initialState;
+  const $nav = document.createElement("nav");
+  $nav.classList.add("category-list");
+  $root.appendChild($nav);
+  this.$target = document.createElement("ul");
+  $nav.appendChild(this.$target);
 
-  category.forEach(([key, value]) => {
-    navList.push(
-      `  
-        <li class="category-item" id="${value.id}">${value.title}</li>   
-      `
-    );
-  });
-  $ul.innerHTML = navList.join("");
-  $nav.appendChild($ul);
-  const $li = document.querySelectorAll("li");
-  $li.forEach((value) => {
-    value.addEventListener("click", function (e) {  
-      let abc =category.find(item => item.id === e.target.id)
-      console.log(category.value);
-      value.classList.add("active");
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
+  };
+
+  const category = Object.entries(this.state);
+
+  this.onClick;
+  this.render = () => {
+    const navList = [];
+    const nodesTemplate = category.forEach(([key, value]) => {
+      navList.push(
+        `
+        <li class="category-item" id="${value.id}">${value.title}</li>
+        `
+      );
     });
-  });
-}
+    this.$target.innerHTML = navList.join("");
+  };
 
-export default Nav;
+  this.render();
+
+  //이벤트 위임?
+  document
+    .querySelector(".category-list")
+    .addEventListener("click", function (e) {
+      e.preventDefault();
+      e.target.classList.add("active");
+      if (e.target.classList.contains("active")) {
+        console.log(123);
+        const id = e.target.getAttribute("id");
+        const active = document.querySelector("active");
+        console.log(id);
+        console.log(e.target.id);
+        //   if (e.target.classList.contains("active")) {
+        //     e.target.classList.add("active");
+        //   }else{
+
+        //   }
+        //   if (id === e.target.classList.contains("active")) {
+        //     console.log(123);
+        //   }
+        //   console.log(e.target.classList.contains("active"));
+      }
+    });
+}
